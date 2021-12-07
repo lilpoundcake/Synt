687ac45f1df2f052c153ccf0a204f7f843d158d9
lilpoundcake <44047612+lilpoundcake@users.noreply.github.com>
2021-12-07 13:24:24 +0700

    Add files via upload

---
+++ file_import.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp CHO.txt ../DNAWorks/
+cp Sf9.txt ../DNAWorks/
+cp bash_next.sh ../DNAWorks/
+cp Logfile_generate.py ../DNAWorks/
+cp Primer_Alg.py ../DNAWorks/
+cp start.sh ../DNAWorks/
