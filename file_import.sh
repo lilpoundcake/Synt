d9487f959778a95f82b904bb982e588cd7c1ce86
lilpoundcake <44047612+lilpoundcake@users.noreply.github.com>
2021-12-07 14:51:56 +0700

    Update file_import.sh

--- file_import.sh
+++ file_import.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+git clone https://github.com/davidhoover/DNAWorks.git
+cd DNAWorks/
+make
+cd ..
+
 cp CHO.txt ../DNAWorks/
 cp Sf9.txt ../DNAWorks/
 cp bash_next.sh ../DNAWorks/
 cp Logfile_generate.py ../DNAWorks/
 cp Primer_Alg.py ../DNAWorks/
 cp start.sh ../DNAWorks/
+
+cd DNAWorks
+bash start.sh
